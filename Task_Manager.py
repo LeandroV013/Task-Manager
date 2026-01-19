4fb0b5edc1d602f0dcf68a3e938d86984a9ad64a
Leandro Vieira <leandrosvieira.contato@gmail.com>
2026-01-19 06:12:19 -0300

    first commit

---
+++ Task_Manager.py
@@ -0,0 +1,25 @@
+# CRUD Create, Read, Update and Delete.
+tarefas = []
+
+def criar_tarefa():
+    print("Uma nova tarefa. Adicionar: Título, Descrição, Status, Data  (Criar/Adicionar) ->  append() ") 
+
+
+
+def listar_tarefas():
+    print('Visualizar. Mostra todos os itens que foram listados. Pode mostrar por data, lista ou mostrar tudo.  (Ler/Listar)->  for + print() ')
+
+
+
+def atualizar_tarefa():
+    print('Adicionar uma nova tarefa a lista já existente  (Atualizar/Editar)  ->  lista[i] = novo ')
+
+
+
+def deletar_tarefa():
+    print('Deletar item, deletar lista, deletar tudo. Botão de confirmação: "TEM CERTEZA QUE DESEJA DELETAR TUDO?" (Deletar/Remover)  ->  pop() ou remove()')
+
+
+
+def menu():
+    print('Mostrar opções e criar funções, nada além disso') 
