0afc5fd7eb1435b71338130a5c8a46ae1055a1aa
Leandro Vieira <leandrosvieira.contato@gmail.com>
2026-01-20 04:16:46 -0300

    Update Task_Manager.py

--- Task_Manager.py
+++ Task_Manager.py
@@ -1,26 +1,61 @@
 # CRUD Create, Read, Update and Delete.
 tarefas = []
 
+
+def linha():
+    print('-'*10)
+
+
 def criar_tarefa():
-    print("Uma nova tarefa. Adicionar: Título, Descrição, Status, Data  (Criar/Adicionar) ->  append() ") 
+    while True:
+        resposta = input('Deseja criar uma nova tarefa? [S/N]: ').lower()
+
+        if resposta == 's':
+            nova_tarefa = input('Adicione uma nova tarefa: ')
+            tarefas.append(nova_tarefa)
+        elif resposta == 'n':
+             break
+        else:
+            print('ENTRADA INVÁLIDA. DIGITE [S/N].')
 
 
-
-def listar_tarefas():
-    print('Visualizar. Mostra todos os itens que foram listados. Pode mostrar por data, lista ou mostrar tudo.  (Ler/Listar)->  for + print() ')
-
+def listar_tarefas(): # Adicionar enumerate
+    if not tarefas:
+        print('Você não tem nenhuma tarefa. ')
+    else:
+        print(tarefas)
 
 
 def atualizar_tarefa():
-    print('Adicionar uma nova tarefa a lista já existente  (Atualizar/Editar)  ->  lista[i] = novo ')
+    pass    # Adicionar uma nova tarefa a lista já existente  (Atualizar/Editar)  ->  lista[i] = novo
 
 
 
 def deletar_tarefa():
-    print('Deletar item, deletar lista, deletar tudo. Botão de confirmação: "TEM CERTEZA QUE DESEJA DELETAR TUDO?" (Deletar/Remover)  ->  pop() ou remove()')
+    pass   # Deletar item, deletar lista, deletar tudo. Botão de confirmação: "TEM CERTEZA QUE DESEJA DELETAR TUDO?" (Deletar/Remover)  ->  pop() ou remove()
 
 
+def menu(): # Mostrar opções e criar funções, nada além disso
+        return input(f'Gerenciador de Tarefas \nSelecione uma opção: [C]riar, [L]istar, [A]tualizar, [D]eletar, [M]enu: ')
 
-def menu():
-    print('Mostrar opções e criar funções, nada além disso') 
-    
+
+while True:
+    select = menu().lower()
+    if not select or select[0] not in 'cladm':
+        print('Selecione uma opção válida. ')
+        continue
+     
+    elif select[0].lower() == 'c':
+         criar_tarefa()
+
+
+    elif select[0].lower() == 'l':
+         listar_tarefas()
+
+
+    elif select[0].lower() == 'a':
+         atualizar_tarefa()
+
+
+    elif select[0].lower() == 'd':
+         deletar_tarefa()
