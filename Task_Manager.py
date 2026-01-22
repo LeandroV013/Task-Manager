88317dd280aa8addee7bee9056b922574057a120
Leandro Vieira <leandrosvieira.contato@gmail.com>
2026-01-21 22:03:35 -0300

    Update Task_Manager.py

--- Task_Manager.py
+++ Task_Manager.py
@@ -26,13 +26,41 @@ def listar_tarefas(): # Adicionar enumerate
         print(tarefas)
 
 
-def atualizar_tarefa():
-    pass    # Adicionar uma nova tarefa a lista já existente  (Atualizar/Editar)  ->  lista[i] = novo
+def atualizar_tarefa():  # TRATAR ERRO DE STRING,CASO O USUÁRIO NÃO COLOQUE O NÚMERO, O CÓDIGO QUEBRA E VAI DAR ERRO DE ENTRADA
+    if not tarefas:
+         print('Você não tem nenhuma tarefa.')
+    else:
+        print('Suas tarefas: ')
+        for i, tarefa in enumerate(tarefas, start=1):
+            print(f'{i}, {tarefa}')
+        
+        indice = int(input('Digite o número da tarefa que deseja editar: '))
+        
+        if indice < 1 or indice > len(tarefas):
+            print('Número inválido.')
+        else:
+            nova_tarefa = input('Digite uma nova tarefa: ')
+            tarefas[indice - 1] = nova_tarefa
+            print('Tarefa atualizada com sucesso!')
 
 
+def deletar_tarefa(): # DELETAR MAIS DE UMA TAREFA POR VEZ QUEBRA O CÓDIGO. COLOCAR OPÇÃO DE DELETAR MAIS DE UM OU DELETAR TUDO.
+    if not tarefas:
+        print('Você não pode apagar porque ainda não tem nenhuma tarefa.')
+    else:
+        print('Suas tarefas: ')
+        for i, tarefa in enumerate(tarefas, start=1):
+            print(f'{i}, {tarefa}')
 
-def deletar_tarefa():
-    pass   # Deletar item, deletar lista, deletar tudo. Botão de confirmação: "TEM CERTEZA QUE DESEJA DELETAR TUDO?" (Deletar/Remover)  ->  pop() ou remove()
+        indice = int(input('Digite o número da tarefa que deseja deletar: '))
+        confirmacao = input(f'Tem certeza que deseja removar a tarefa "{indice}" [S/N]? ').lower()
+        if confirmacao == 's':
+            if indice < 1 or indice > len(tarefas):
+                print('Número inválido.')
+            else:
+                tarefa_removida = tarefas.pop(indice - 1)
+                print(f'Tarefa "{tarefa_removida}" removida com sucesso!')
+
 
 
 def menu(): # Mostrar opções e criar funções, nada além disso
